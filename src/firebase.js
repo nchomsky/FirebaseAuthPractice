40eb30cdef5e7fd2d3f1ff223c2b67520f37a671
Noah Chomsky <nbchomp03@gmail.com>
2021-03-12 20:22:31 -0500

    Install bootstrap and remove default files

--- src/firebase.js
+++ src/firebase.js
@@ -9,3 +9,6 @@ const app = firebase.initializeapp({
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
+
+export const auth = app.auth();
+export default app;
